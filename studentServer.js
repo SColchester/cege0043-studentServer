822fcd719419797d68ba7156dcd57282ad2218b7
SColchester <scolch@gmail.com>
2019-02-28 07:08:36 -0500

    Added GET functionality to studentServer.js file

--- studentServer.js
+++ studentServer.js
@@ -9,8 +9,11 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
-app.get('/',function (req,res) {
-	res.send("hello world from the HTTP server");
+app.get('/test.html',function (req,res) {
+	// run some server-side code
+	console.log('test.html requested');
+	// note that __dirname gives the path to the studentServer.js file
+	res.sendFile(__dirname + '/test.html');
 });
 // adding functionality to log the requests - useful for debugging
 app.use(function (req, res, next) {
